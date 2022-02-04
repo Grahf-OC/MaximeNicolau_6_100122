a58130111f20691dbfe921383805fe58941cb4e4
Grahf-OC <maximenicolau33@gmail.com>
2022-02-04 19:07:24 +0100

    ajout password validator

--- controllers/user.js
+++ controllers/user.js
@@ -1,23 +1,41 @@
 const bcrypt = require("bcrypt");
 const User = require("../models/User");
+const passwordValidator = require('password-validator');
 const jwt = require("jsonwebtoken");
 
 require("dotenv").config();
 
+
+const pwSchema = new passwordValidator();
+
+pwSchema
+  .is().min(8)                                    // Minimum length 8
+  .is().max(100)                                  // Maximum length 100
+  .has().uppercase()                              // Must have uppercase letters
+  .has().lowercase()                              // Must have lowercase letters
+  .has().digits(2)                                // Must have at least 2 digits
+  .has().not().spaces()                           // Should not have spaces
+  .is().not().oneOf(['Passw0rd', 'Password123', '1234']);
+
+
 exports.signup = (req, res, next) => {
-  bcrypt
-    .hash(req.body.password, 10)
-    .then((hash) => {
-      const user = new User({
-        email: req.body.email,
-        password: hash,
-      });
-      user
-        .save()
-        .then(() => res.status(201).json({ message: "Utilisateur créé !" }))
-        .catch((error) => res.status(400).json({ error }));
-    })
-    .catch((error) => res.status(500).json({ error }));
+  if (!pwSchema.validate(req.body.password)) {
+    res.status(401).json({ message: "Le mot de passe doit contenir entre 8 et 100 caractères, doit avoir des minuscules et des majuscules, ainsi qu'au moins 2 chiffres et pas d'espace." });
+  } else {
+    bcrypt
+      .hash(req.body.password, 10)
+      .then((hash) => {
+        const user = new User({
+          email: req.body.email,
+          password: hash,
+        });
+        user
+          .save()
+          .then(() => res.status(201).json({ message: "Utilisateur créé !" }))
+          .catch((error) => res.status(400).json({ error }));
+      })
+      .catch((error) => res.status(500).json({ error }));
+  }
 };
 
 exports.login = (req, res, next) => {
@@ -37,7 +55,7 @@ exports.login = (req, res, next) => {
             token: jwt.sign({ userId: user._id }, process.env.TOKEN, {
               expiresIn: "24h",
             }),
-          }); 
+          });
         })
         .catch((error) => res.status(500).json({ error }));
     })
